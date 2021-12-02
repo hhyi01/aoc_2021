4a605517470effecb98b9acc5b21c2e314621f44
hhyi01 <helen.yi@moodys.com>
2021-12-02 13:28:00 -0800

    Day 2 solutions

--- day02.go
+++ day02.go
@@ -1,1 +1,61 @@
 package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func day02Part1(filePath string)  {
+	puzzleInput := readFileLines(filePath)
+	horizontal, depth := 0, 0
+	for _, line := range puzzleInput {
+		command := strings.Split(line, " ")
+		direction := command[0]
+		units, _ := strconv.Atoi(command[1])
+		horizontal, depth = day02Part1CalcPosition(direction, units, horizontal, depth)
+	}
+	fmt.Println("Part 1 - final horizontal x final depth = ", horizontal * depth)
+}
+
+func day02Part1CalcPosition(direction string, units int, horizontal int, depth int) (int, int) {
+	switch direction {
+	case "forward":
+		horizontal += units
+	case "down":
+		depth += units
+	case "up":
+		depth -= units
+	default:
+		fmt.Errorf("unknown direction")
+	}
+	return horizontal, depth
+}
+
+func day02Part2(filePath string)  {
+	puzzleInput := readFileLines(filePath)
+	horizontal, depth, aim := 0, 0, 0
+	for _, line := range puzzleInput {
+		command := strings.Split(line, " ")
+		direction := command[0]
+		units, _ := strconv.Atoi(command[1])
+		horizontal, depth, aim = day02Part2CalcPosition(direction, units, horizontal, depth, aim)
+	}
+	fmt.Println("Part 2 - final horizontal x final depth = ", horizontal * depth)
+}
+
+func day02Part2CalcPosition(direction string, units int, horizontal int, depth int, aim int) (int, int, int) {
+	switch direction {
+	case "forward":
+		horizontal += units
+		depth += aim * units
+	case "down":
+		aim += units
+	case "up":
+		aim -= units
+	default:
+		fmt.Errorf("unknown direction")
+	}
+	return horizontal, depth, aim
+}
+
--- utils.go
+++ utils.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func readFileLines(filePath string) []string {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
+	scanner := bufio.NewScanner(file)
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	var fileInput []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		fileInput = append(fileInput, line)
+	}
+	return fileInput
+}
