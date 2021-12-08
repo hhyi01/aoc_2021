dc83abf3a7db42cf1cb00c3b05a99d048bbee929
hhyi01 <helen.yi@moodys.com>
2021-12-07 16:13:23 -0800

    Day 7 solutions

---
+++ day07.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+func day07Part1(filePath string)  {
+	puzzleInput := readFileLines(filePath)
+	positions := strings.Split(puzzleInput[0], ",")
+	minPos := calcMinPosition(positions)
+	maxPos := calcMaxPosition(positions)
+	fuelCalcs := make(map[int]int)
+	for pos := minPos; pos <= maxPos; pos++ {
+		totalFuel := 0
+		for _, v := range positions {
+			crabSubPos, _ := strconv.Atoi(v)
+			_, found := fuelCalcs[pos]
+			if !found {
+				fuel := pos - crabSubPos
+				if fuel < 0 {
+					fuel = fuel * -1
+				}
+				totalFuel += fuel
+			}
+		}
+		_, found := fuelCalcs[pos]
+		if !found {
+			fuelCalcs[pos] = totalFuel
+		}
+	}
+	leastFuelPos, fuelConsumed := calcMinFuel(fuelCalcs)
+	fmt.Println("Horizontal position using least fuel =", leastFuelPos)
+	fmt.Println("Part 1 - fuel spent to align to horizontal position =", fuelConsumed)
+}
+
+func day07Part2(filePath string)  {
+	puzzleInput := readFileLines(filePath)
+	positions := strings.Split(puzzleInput[0], ",")
+	minPos := calcMinPosition(positions)
+	maxPos := calcMaxPosition(positions)
+	fuelCalcs := make(map[int]int)
+	for pos := minPos; pos <= maxPos; pos++ {
+		totalFuel := 0
+		for _, v := range positions {
+			crabSubPos, _ := strconv.Atoi(v)
+			_, found := fuelCalcs[pos]
+			if !found {
+				fuel := pos - crabSubPos
+				if fuel < 0 {
+					fuel = fuel * -1
+				}
+				crabFuelCost := crabCalcFuel(1, fuel, fuel)
+				totalFuel += crabFuelCost
+			}
+		}
+		_, found := fuelCalcs[pos]
+		if !found {
+			fuelCalcs[pos] = totalFuel
+		}
+	}
+	leastFuelPos, fuelConsumed := calcMinFuel(fuelCalcs)
+	fmt.Println("Horizontal position using least fuel =", leastFuelPos)
+	fmt.Println("Part 2 - fuel spent to align to horizontal position =", fuelConsumed)
+}
+
+func calcMinPosition(crabPositions []string) int {
+	minPos := math.MaxInt
+	for _, v := range crabPositions {
+		pos, _ := strconv.Atoi(v)
+		if pos < minPos {
+			minPos = pos
+		}
+	}
+	return minPos
+}
+
+func calcMaxPosition(crabPositions []string) int {
+	maxPos := math.MinInt
+	for _, v := range crabPositions {
+		pos, _ := strconv.Atoi(v)
+		if pos > maxPos {
+			maxPos = pos
+		}
+	}
+	return maxPos
+}
+
+func calcMinFuel(fuelCalcs map[int]int) (int, int) {
+	minFuel := math.MaxInt
+	minFuelPos := 0
+	for k, v := range fuelCalcs {
+		if v < minFuel {
+			minFuel = v
+			minFuelPos = k
+		}
+	}
+	return minFuelPos, minFuel
+}
+
+func crabCalcFuel(start int, end int, numTerms int) int {
+	sum := numTerms * (start + end) / 2
+	return sum
+}
